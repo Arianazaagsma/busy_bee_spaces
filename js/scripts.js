15709bd41bc87c40e489e319c463f1452058bafb
Ariana Zaagsma <arianazaagsma@gmail.com>
2022-09-09 14:37:48 -0700

    spaces page

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,16 @@ $(function() {
     $("#bookTourButton").click(function(){
         $("#bookTourModal").modal()
     });
+    $("#mainSpacesButton").click(function(){
+        $("#mainSpacesModal").modal()
+    });
+    $("#plantRoomButton").click(function(){
+        $("#plantRoomModal").modal()
+    });
+    $("#meetingButton").click(function(){
+        $("#meetingModal").modal()
+    });
+    $("#deskButton").click(function(){
+        $("#deskModal").modal()
+    });
 });
